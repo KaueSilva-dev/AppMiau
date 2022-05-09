5bf569dcc68d875339e6893544bd1b89c90eb05a
kawps90 <kaue.ps90@protonmail.com>
2022-05-09 01:17:38 -0300

    estruturação, navbar, componentes principais

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { Section1Component } from './section1/section1.component';
+import { Section2Component } from './section2/section2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    Section1Component,
+    Section2Component
   ],
   imports: [
     BrowserModule,
